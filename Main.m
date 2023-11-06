be7639dacf97dc9b93d43a32992b9ba2d05b7424
vlazed <72566071+lavzed@users.noreply.github.com>
2023-11-05 23:30:48 -0500

    Added missing spec point logic
    Use geoplot to plot specular points and missing specular points

--- Main.m
+++ Main.m
@@ -8,13 +8,15 @@ clear; clc;
 angle = 60;
 
 % number of recievers
-n_rx = 4;
+n_rx = 9;
 
 % target folder in outputs folder
 target_folderL5 = strcat(num2str(n_rx), '_400_80_1_L5');
 target_folderL1 = strcat(num2str(n_rx), '_400_80_1_L1');
 
 %% INITIALIZATION
+missedNum = 1;
+
 success_count = 0;
 fail_count = 0; 
 
@@ -32,13 +34,19 @@ latPer25Km = res*latPerKm;
 
 lati = -90;
 
+missingSpecsTot = zeros(length(spec),2);
+
 %% CALCULATION
 while lati < (90 - latPer25Km)
     
     latNext = lati+latPer25Km;
     longes = GenerateGridPoints(lati,latNext,res);
     
-    [si,fi] = CheckPointsInGrid(lati,latNext,longes,spec);
+    [si,fi, missingSpecs] = CheckPointsInGrid(lati,latNext,longes,spec);
+    if ~isempty(missingSpecs)
+        missingSpecsTot(missedNum:missedNum + length(longes)-2,:) = missingSpecs;
+        missedNum = missedNum+length(longes)-1;
+    end
     success_count = success_count +si;
     fail_count = fail_count + fi;
     lati = latNext;
@@ -50,6 +58,14 @@ PercentCoverage = success_count/(success_count+fail_count) * 100;
 
 %% OUTPUT
 
+idxes = find(missingSpecsTot(:,1));
+actualMissingSpecs = zeros(length(idxes),2);
+for m = 1:1:length(idxes)
+    actualMissingSpecs(m,:) = [missingSpecsTot(idxes(m),1),missingSpecsTot(idxes(m),2)];
+end
+
 fprintf("Done! Percentage Coverage of %2.0f Rx: %2.2f%%\n", n_rx, PercentCoverage);
 
-geoplot(spec(:,1), spec(:,2), '.', 'MarkerSize', 1)
+geoplot(spec(:,1), spec(:,2), '.', 'MarkerSize', 1, 'Color', [0 0 1])
+hold on
+geoplot(actualMissingSpecs(:,1), actualMissingSpecs(:,2), '.', 'MarkerSize', 1, 'Color', [1 0 0])
